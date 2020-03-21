c33244907fba857e0c1ba722fb3370cef235b372
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-21 23:28:20 +1000

    Add logic for get match key and add template clip for testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles/main.scss';
+import matchCode from './matchCode.js';
 
 class App extends React.Component {
   constructor() {
@@ -13,10 +14,14 @@ class App extends React.Component {
   }
 
   playSound(e, method) {
-    // to do: define the condition that match the value or keyCode
     let value = method ==="click"? e.target.value : e.keyCode;
-    console.log(value);
-    console.log("playSound...")
+    const getCode = matchCode.find((element) => {
+      return element.key === value || element.keyCode === value;
+    })
+    console.log("playSound...");
+    if(getCode!==undefined){
+      document.querySelector('#'+ getCode.key).play()
+    };
   }
 
   render() {
@@ -24,31 +29,31 @@ class App extends React.Component {
       <div className="App">Drum Machine FCC
         <div id="drum-machine">
           <button className="drum-pad" onClick={(e)=>this.playSound(e, "click")} value="Q">Q
-            <audio src="" className="clip" id="Q"></audio>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3" className="clip" id="Q"></audio>
           </button>
           <button className="drum-pad" onClick={(e)=>this.playSound(e, "click")} value="W">W
-            <audio src="" className="clip" id="W"></audio>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3" className="clip" id="W"></audio>
           </button>
           <button className="drum-pad" onClick={(e)=>this.playSound(e, "click")} value="E">E
-            <audio src="" className="clip" id="E"></audio>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3" className="clip" id="E"></audio>
           </button>
           <button className="drum-pad" onClick={(e)=>this.playSound(e, "click")} value="A">A
-            <audio src="" className="clip" id="A"></audio>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3" className="clip" id="A"></audio>
           </button>
           <button className="drum-pad" onClick={(e)=>this.playSound(e, "click")} value="S">S
-            <audio src="" className="clip" id="S"></audio>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3" className="clip" id="S"></audio>
           </button>
           <button className="drum-pad" onClick={(e)=>this.playSound(e, "click")} value="D">D
-            <audio src="" className="clip" id="D"></audio>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3" className="clip" id="D"></audio>
           </button>
           <button className="drum-pad" onClick={(e)=>this.playSound(e, "click")} value="Z">Z
-            <audio src="" className="clip" id="Z"></audio>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3" className="clip" id="Z"></audio>
           </button>
           <button className="drum-pad" onClick={(e)=>this.playSound(e, "click")} value="X">X
-            <audio src="" className="clip" id="X"></audio>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3" className="clip" id="X"></audio>
           </button>
           <button className="drum-pad" onClick={(e)=>this.playSound(e, "click")} value="C">C
-            <audio src="" className="clip" id="C"></audio>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3" className="clip" id="C"></audio>
           </button>
         </div>
       </div>
