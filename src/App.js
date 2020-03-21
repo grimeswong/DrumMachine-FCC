6c01e0bca8b5695063ad11270b3cc012c8c6a3e1
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-21 21:52:58 +1000

    Define the user story required tags and structure

--- src/App.js
+++ src/App.js
@@ -3,8 +3,36 @@ import './styles/main.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Create React App Base with Sass</h1>
+    <div className="App">Drum Machine FCC
+      <div id="drum-machine">
+        <button className="drum-pad">Q
+          <audio src="" className="clip" id="id1"></audio>
+        </button>
+        <button className="drum-pad">W
+          <audio src="" className="clip" id="id2"></audio>
+        </button>
+        <button className="drum-pad">E
+          <audio src="" className="clip" id="id3"></audio>
+        </button>
+        <button className="drum-pad">A
+          <audio src="" className="clip" id="id4"></audio>
+        </button>
+        <button className="drum-pad">S
+          <audio src="" className="clip" id="id5"></audio>
+        </button>
+        <button className="drum-pad">D
+          <audio src="" className="clip" id="id6"></audio>
+        </button>
+        <button className="drum-pad">Z
+          <audio src="" className="clip" id="id7"></audio>
+        </button>
+        <button className="drum-pad">X
+          <audio src="" className="clip" id="id8"></audio>
+        </button>
+        <button className="drum-pad">C
+          <audio src="" className="clip" id="id9"></audio>
+        </button>
+      </div>
     </div>
   );
 }
